a71eef6c7d320e6f49676e81a634cc7bde5e3e96
Alan Mesquita <alan.mdesousa@gmail.com>
2015-05-22 18:14:14 -0300

    adding model

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var SlackService = require('./lib/slack-service.js')
 
+mongoose = require('mongoose');
+
 config = require("./config/config.json");
+root   = __dirname;
+
+mongoose.connect(config.db.host);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 var slackService = new SlackService(config.slack.token);
 slackService.run();
